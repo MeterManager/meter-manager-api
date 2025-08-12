74b00cd0ae3c0b69e9ab6ed141e66601495d741e
balaziuk <balaztasi@gmail.com>
2025-08-12 21:36:15 +0300

    add test route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const locationRoutes = require('./locationRoutes');
+const testRoute = require('./testRoute');
 
 router.use('/locations', locationRoutes);
+router.use('/test', testRoute);
 
 module.exports = router;

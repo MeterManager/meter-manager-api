d6c76026c53e4e4f01dc2c6f2bcb2aa7df560bcd
AlinaPavlenk18 <alinapavlenk01005@gmail.com>
2025-08-29 10:50:35 +0300

    Implemented tariff-service with Swagger documentation

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const locationRoutes = require('./locationRoutes');
 const testRoute = require('./testRoute');
 const resourceTypeRoutes = require('./resourceTypeRoutes');
+const tariffRoutes = require('./tariffRoutes');
 
 router.use('/locations', locationRoutes);
 router.use('/test', testRoute);
 router.use('/resource-types', resourceTypeRoutes);
+router.use('/tariffs', tariffRoutes);
 
 module.exports = router;

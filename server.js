6a3ba14d6eb8cf6280065ae36c20ae2b67b5eae9
tania-macheieva <macheieva.tetiana@gmail.com>
2025-12-07 19:19:52 +0200

    chore: update configs

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({
+  path: path.resolve(__dirname, `.env.${process.env.NODE_ENV || 'development'}`)
+});
 
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
